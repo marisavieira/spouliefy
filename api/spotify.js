9b7f209926e682f3140acc9397a5151ff4b6bc8f
Marisa Vieira <58489737+marisavieira@users.noreply.github.com>
2025-11-26 16:48:16 +0000

    test

--- api/spotify.js
+++ api/spotify.js
@@ -402,7 +402,7 @@ async function handleCallback(req, res, code) {
     <section class="card">
       <div class="left" aria-hidden="true">
         <!-- TROCAR PELA SUA IMAGEM -->
-        <img src="./image/poulie-spotify-girl.png" alt="">
+        <img src="./image/poulieshop_site.png" alt="">
       </div>
 
       <div class="right">
